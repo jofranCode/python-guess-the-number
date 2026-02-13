ff6d9c7eb9305e73a72a59e54de033bcb98b33b8
jofranCode <jofranCode@proton.me>
2026-02-12 22:14:47 -0300

    feat: implement user name input

---
+++ python-guess-the-number.py
@@ -0,0 +1,9 @@
+import random
+
+attempt_count = 0
+number = random.randint(1, 20)
+
+print("What is your name?")
+user_name = input('-->')
+
+print(f"Well, {user_name}, I'm thinking of a number between 1 and 20")
